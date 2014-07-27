1eab2e26400a2289b7969a61337f2c675e44af2a
jason_yao <jason_yao@htc.com>
2014-07-28 01:34:43 +0800

    update

---
+++ example/嵌入式Linux驅動模板精講與項目實踐/第6章/select and poll/poll.h
@@ -0,0 +1,12 @@
+
+#ifndef _POLL_H_
+#define _POLL_H_
+
+struct poll_dev                                     
+{     
+    bool   flag;
+    wait_queue_head_t wq;      
+    struct timer_list timer;
+};
+
+#endif 
